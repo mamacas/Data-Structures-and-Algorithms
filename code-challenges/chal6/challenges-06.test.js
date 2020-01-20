e613a9840061da2d4076a239978da6b7e451f772
CAS OLEJNICZAK <CASOLEJNICZAK@GMAIL.COM>
2020-01-19 21:37:19 -0800

    added file for challenges-06

--- code-challenges/chal6/challenges-06.test.js
+++ code-challenges/chal6/challenges-06.test.js
@@ -14,6 +14,7 @@ const courseInfo = { name: 'Code 301', duration: { dayTrack: '4 weeks', eveningT
 
 const getCourseKeys = (obj) => {
   // Solution code here...
+  //typesomethingasatest
 };
 
 /* ------------------------------------------------------------------------------------------------
