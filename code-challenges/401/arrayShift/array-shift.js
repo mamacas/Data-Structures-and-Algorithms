0372a39c3d5a694e83d53c424196a296c4cde959
Cas Ibrahim <qasiraibrahim@gmail.com>
2020-08-11 21:32:16 -0700

    solved challenge for class 02

---
+++ code-challenges/401/arrayShift/array-shift.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// Write a function called insertShiftArray which takes in an array and the value to be added. Without utilizing any of the built-in methods available to your language, return an array with the new value added at the middle index.
+
+let array = [1, 2, 3, 4, 5, 6];
+
+const insertShiftArray = (arr, val) => {
+
+  // capture middle index in arr
+  // if array.length % 2 !== 0, 
+  // use Math.ceil to round up to nearest integer
+  let mid = Math.ceil(arr.length / 2);
+
+  // create output array
+  let arr2 = [];
+
+  // iterate over arr
+  // when we reach the middle index, push val to the result array
+  // then continue iterating over arr, 
+  // pushing the remaining values into the result array
+  for (let i = 0; i < arr.length; i++) {
+    if (i === mid) {
+      arr2.push(val);
+    }
+    arr2.push(arr[i]);
+  }
+
+  return arr2;
+
+};
+
+console.log(insertShiftArray(array, 100));
+
+module.exports = insertShiftArray();
