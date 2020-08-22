5a05c3ad14183ba77e5e0e5fe071929121fce674
Cas Ibrahim <qasiraibrahim@gmail.com>
2020-08-21 23:25:49 -0700

    finished challenges for class 10

--- data-structures/__test__/stacks-queues.test.js
+++ data-structures/__test__/stacks-queues.test.js
@@ -1,14 +1,33 @@
-const Node = require('../stacksAndQueues/stacks-and-queues.js');
-const Stack = require('../stacksAndQueues/stacks-and-queues.js');
-// const Queue = require('../stacksAndQueues/stacks-and-queues.js');
+const Node = require('../stacksAndQueues/stacks-and-queues.js').Node;
 
-it('Nodes, Stacks and Queues should be classes', () => {
-  expect(Node).toBeDefined();
-  expect(Stack).toBeDefined();
-  // expect(Queue).toBeDefined();
+const Stack = require('../stacksAndQueues/stacks-and-queues.js').Stack;
+
+const Queue = require('../stacksAndQueues/stacks-and-queues.js').Queue;
+
+// const test = require('../stacksAndQueues/stacks-and-queues.js');
+
+describe('Node', () => {
+
+  it('should be a class', () => {
+    expect(Node).toBeDefined();
+  });
+
+  it('should contain a value property and a next property', () => {
+    const node = new Node();
+    node.value = 123;
+    node.next = 456;
+    expect(node.value).toBeDefined();
+    expect(node.next).toBeDefined();
+  });
+
 });
 
+
 describe('Stacks', () => {
+
+  it('should be a class', () => {
+    expect(Stack).toBeDefined();
+  });
 
   it('should instantiate an empty stack', () => {
     const stack = new Stack();
@@ -17,8 +36,9 @@ describe('Stacks', () => {
 
   it('should successfully push onto a stack', () => {
     const stack = new Stack();
+    stack.push('abc');
     stack.push('xyz');
-    expect(stack.head.value).toBe('xyz');
+    expect(stack.top.value).toBe('xyz');
   });
 
   it('should successfully pop off from a stack containing only one node', () => {
@@ -36,6 +56,19 @@ describe('Stacks', () => {
     const popped = stack.pop();
     expect(popped).toBe('xyz');
     expect(stack.isEmpty()).toBeFalsy();
+  });
+
+  it('should successfully pop off from a stack until the stack is empty', () => {
+    const stack = new Stack();
+    stack.push('xyz');
+    stack.push('abc');
+    stack.push('rst');
+    stack.push('lmn');
+    stack.pop();
+    stack.pop();
+    stack.pop();
+    stack.pop();
+    expect(stack.isEmpty()).toBeTruthy();
   });
 
   it('should not allow popping from empty stack', () => {
@@ -69,4 +102,82 @@ describe('Stacks', () => {
 
 });
 
+describe('Queue', () => {
 
+  it('should be a class', () => {
+    expect(Queue).toBeDefined();
+  });
+
+  it('should instantiate an empty queue', () => {
+    const queue = new Queue();
+    expect(queue).toBeDefined();
+  });
+
+  it('should return true if queue is empty', () => {
+    const queue = new Queue();
+    expect(queue.isEmpty()).toBeTruthy;
+  });
+
+  it('should return false if the queue is not empty', () => {
+    const queue = new Queue();
+    queue.enqueue('abc');
+    expect(queue.isEmpty()).toBeFalsy;
+  });
+
+  it('should successfully enqueue into an empty queue', () => {
+    const queue = new Queue();
+    queue.enqueue('abc');
+    expect(queue.front.value).toBe('abc');
+    expect(queue.rear.value).toBe('abc');
+  });
+
+  it('should successfully enqueue into the rear position of a queue with existing values', () => {
+    const queue = new Queue();
+    queue.enqueue('abc');
+    queue.enqueue('rst');
+    queue.enqueue('xyz');
+    expect(queue.front.value).toBe('abc');
+    expect(queue.front.next.value).toBe('rst');
+    expect(queue.rear.value).toBe('xyz');
+    expect(queue.rear.next).toBe(null);
+  });
+
+  it('should peek at the front node in the queue', () => {
+    const queue = new Queue();
+    queue.enqueue('abc');
+    queue.enqueue('xyz');
+    expect(queue.peek()).toBe('abc');
+  });
+
+  it('should not allow peeking an empty queue', () => {
+    const queue = new Queue();
+    expect(() => queue.peek()).toThrow(Error);
+    expect(() => queue.peek()).toThrow('cannot peek empty queue');
+  });
+
+  it('should dequeue from the front of the queue if there is only one node in the queue', () => {
+    const queue = new Queue();
+    queue.enqueue('xyz');
+    let popped = queue.dequeue();
+    expect(popped).toBe('xyz');
+    expect(queue.isEmpty()).toBeTruthy;
+  });
+
+  it('should dequeue from the front of the queue if there are multiple nodes in the queue', () => {
+    const queue = new Queue();
+    queue.enqueue('xyz');
+    queue.enqueue('abc');
+    queue.enqueue('rst');
+    queue.enqueue('lmn');
+    let popped = queue.dequeue();
+    expect(popped).toBe('xyz');
+    expect(queue.isEmpty()).toBeFalsy;
+  });
+
+  it('should not allow dequeueing from an empty queue', () => {
+    const queue = new Queue();
+    expect(() => queue.dequeue()).toThrow(Error);
+    expect(() => queue.dequeue()).toThrow('cannot dequeue from empty queue');
+  });
+
+});
--- data-structures/stacksAndQueues/stacks-and-queues.js
+++ data-structures/stacksAndQueues/stacks-and-queues.js
@@ -1,3 +1,5 @@
+'use strict';
+
 class Node {
   constructor(value, next) {
     this.value = value;
@@ -8,7 +10,7 @@ class Node {
 class Stack {
 
   constructor() {
-    this.head = null;
+    this.top = null;
   }
 
   peek() {
@@ -17,12 +19,13 @@ class Stack {
       throw new Error('cannot peek empty stack');
     }
 
-    return this.head.value;
+    return this.top.value;
 
   }
 
   push(value) {
-    this.head = new Node(value, this.head);
+
+    this.top = new Node(value, this.top);
   }
 
   pop() {
@@ -31,35 +34,102 @@ class Stack {
       throw new Error('cannot pop empty stack');
     }
 
-    if (!this.head.next) {
-      let temp = this.head;
-      this.head = null;
+    let temp = this.top;
+
+    // if only one node in stack:
+    if (!this.top.next) {
+      this.top = null;
       return temp.value;
     }
 
-    if (this.head.next) {
-      let temp = this.head;
-      temp.next = this.head.next;
-      console.log(temp.next.value);
-      this.head = null;
+    // if more than one node in stack:
+    if (this.top.next) {
+      this.top = this.top.next;
       return temp.value;
     }
+
   }
 
   isEmpty() {
-    if (this.head) {
+
+    if (this.top) {
       return false;
     }
 
     return true;
+
   }
 
 }
 
-// class Queue {
+class Queue {
 
-// }
+  constructor() {
+    this.front = null;
+    this.rear = null;
+  }
 
-module.exports = Node;
-module.exports = Stack;
-// module.exports = Queue;
+  enqueue(value) {
+
+    if (this.isEmpty()) {
+
+      let temp = new Node(value, null);
+      this.front = temp;
+      this.rear = temp;
+      return;
+
+    }
+
+    let temp = new Node(value, null);
+    this.rear.next = temp;
+    this.rear = temp;
+
+  }
+
+  peek() {
+
+    if (this.isEmpty()) {
+      throw new Error('cannot peek empty queue');
+    }
+
+    return this.front.value;
+
+  }
+
+  dequeue() {
+
+    if (this.isEmpty()) {
+      throw new Error('cannot dequeue from empty queue');
+    }
+
+    let temp = this.front;
+
+    // if only one node in queue:
+    if (!this.front.next) {
+      this.front = null;
+      return temp.value;
+    }
+
+    // if more than one node in queue:
+    if (this.front.next) {
+      this.front = this.front.next;
+      return temp.value;
+    }
+
+  }
+
+  isEmpty() {
+
+    if (this.front) {
+      return false;
+    }
+
+    return true;
+
+  }
+
+
+}
+
+module.exports = { Node, Stack, Queue };
+
