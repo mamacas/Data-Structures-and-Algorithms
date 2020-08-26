7ecfb69bff92e57e7fd5cecd3c7b172c27b8f38e
Cas Ibrahim <qasiraibrahim@gmail.com>
2020-08-25 19:07:48 -0700

    solved challenges for class 11

--- data-structures/fifoAnimalShelter/fifo-animal-shelter.js
+++ data-structures/fifoAnimalShelter/fifo-animal-shelter.js
@@ -1,25 +1,78 @@
 'use strict';
 
-const Node = require('../linkedList/linked-list.js');
+class Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
 
 class AnimalShelter {
 
   constructor() {
-    this.dog = new Node('dog', null);
-    this.cat = new Node('cat', null);
+    this.front = null;
+    this.rear = null;
   }
 
   enqueue(animal) {
+    if (!this.rear) {
+      let temp = new Node(animal, null);
+      this.front = temp;
+      this.rear = temp;
+      return;
+    }
 
+    let temp = new Node(animal, null);
+    this.rear.next = temp;
+    this.rear = temp;
   }
 
   dequeue(pref) {
+
+    // if the queue is empty
+    if (!this.rear) {
+      throw new Error('cannot dequeue from an empty queue');
+    }
+
+    let temp = this.front;
+
+    // if only one animal in queue &
+    // animal type matches input pref
+    if (!this.front.next && this.front.value === pref) {
+      this.front = null;
+      return temp.value;
+    }
+
+    // if only one animal in queue &
+    // animal type does not match input pref
+    if (!this.front.next && this.front.value !== pref) {
+      return null;
+    }
+
+    // if more than one animal in queue &
+    // animal type at the front node matches input pref
+    if (this.front.next && this.front.value === pref) {
+      this.front = this.front.next;
+      return temp.value;
+    }
+
+    // if more than one animal in queue &
+    // animal type at the front does not match input pref
+    if (this.front.next && this.front.value !== pref) {
+      while (this.front.value !== pref) {
+        this.front = this.front.next;
+      }
+      return this.front.value;
+    }
+
+    // if the value passed for pref is
+    // neither a cat nor a dog
+    if (pref !== 'dog' && pref !== 'cat') {
+      return null;
+    }
 
   }
 
 }
 
 module.exports = AnimalShelter;
-
-
-
