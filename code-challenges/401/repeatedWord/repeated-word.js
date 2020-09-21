e67a4768eb0449a4433df9ae96d2da3c41ec9381
Cas Ibrahim <qasiraibrahim@gmail.com>
2020-09-21 15:26:16 -0700

    solved challenges for class 31

---
+++ code-challenges/401/repeatedWord/repeated-word.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const checkRepeats = (str) => {
+
+  // takes out the following characters:
+  // '.'
+  // '/'
+  // ','
+  // '!'
+  // '-'
+  // '_'
+  // '&'
+  // '+'
+  let newSTR = str.replace(/[\d\.\/,!\-_&\+]/g, '');
+
+  // removes extra spaces created as a result of removing special characters
+  let nospaceSTR = newSTR.replace(/\s{2,}/g, ' ');
+
+  // sets string to upper case to account for case differences
+  let finalSTR = nospaceSTR.toLowerCase();
+
+  let arr = finalSTR.split(' ');
+
+  let obj = {};
+
+  for (let i = 0; i < arr.length; i++) {
+
+    // if array value already exists as an object key, it means it is the first repeated word in the array
+    if (obj[arr[i]]) {
+      return arr[i];
+    }
+
+    // set array value at i to object key if it does not already exist as a key in the object
+    obj[arr[i]] = 1;
+  }
+
+  // if we loop through the entire array, it means there are no repeated values
+  return 'No repeated words in this string';
+
+};
+
+module.exports = checkRepeats;
