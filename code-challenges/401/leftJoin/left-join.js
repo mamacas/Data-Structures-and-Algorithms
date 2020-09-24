070394436080fb3cac23e150a2930353592370d0
Cas Ibrahim <qasiraibrahim@gmail.com>
2020-09-24 00:13:58 -0700

    removed console logs

--- code-challenges/401/leftJoin/left-join.js
+++ code-challenges/401/leftJoin/left-join.js
@@ -1,13 +1,10 @@
 'use strict';
 
 const leftJoin = (leftObj, rightObj) => {
-  console.log(leftObj);
-  console.log(rightObj);
 
   const matrix = [];
 
   let keys = Object.keys(leftObj);
-  console.log(keys);
 
   for (let i = 0; i < keys.length; i++) {
     const arr = [];
@@ -27,9 +24,6 @@ const leftJoin = (leftObj, rightObj) => {
 
     matrix.push(arr);
   }
-
-  console.log(matrix);
-
 
   return matrix;
 };
