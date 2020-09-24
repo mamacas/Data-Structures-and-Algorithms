433a4b26bf1948673ba2f44da376145ffafa8a28
Cas Ibrahim <qasiraibrahim@gmail.com>
2020-09-24 09:21:14 -0700

    removed sample test objects and implemented challenge requirements re: antonyms and synonyms

--- code-challenges/401/__test__/left-join.test.js
+++ code-challenges/401/__test__/left-join.test.js
@@ -6,15 +6,15 @@ describe('Left Join', () => {
 
   it('should return an array of arrays containing the object key at index 0, the value from the left object at index 1, and the value from the right object at index 2', () => {
 
-    const obj1 = { key: 'anotha', key2: 'we the best', key3: 'dj' };
-    const obj2 = { key: 'one', key2: 'music', key3: 'khaled' };
+    const obj1 = { accessible: 'reachable', service: 'assistance', productive: 'effective' };
+    const obj2 = { accessible: 'restricted', service: 'hindrance', productive: 'lagging' };
 
     const result = leftJoin(obj1, obj2);
     expect(result).toEqual(
       [
-        [ 'key', 'anotha', 'one' ],
-        [ 'key2', 'we the best', 'music' ],
-        [ 'key3', 'dj', 'khaled' ],
+        [ 'accessible', 'reachable', 'restricted' ],
+        [ 'service', 'assistance', 'hindrance' ],
+        [ 'productive', 'effective', 'lagging' ]
       ]
     );
 
@@ -22,32 +22,33 @@ describe('Left Join', () => {
 
   it('should work if there are keys present in the left object which do not exist in the right object, returning null at index 2 of the inner array for that key', () => {
 
-    const obj1 = { key: 'anotha', key2: 'we the best', key3: 'dj', key4: 'asahd' };
-    const obj2 = { key: 'one', key2: 'music', key3: 'khaled' };
+    const obj1 = { accessible: 'reachable', service: 'assistance', productive: 'effective', innovation: 'invention' };
+    const obj2 = { accessible: 'restricted', service: 'hindrance', productive: 'lagging' };
 
     const result = leftJoin(obj1, obj2);
     expect(result).toEqual(
       [
-        [ 'key', 'anotha', 'one' ],
-        [ 'key2', 'we the best', 'music' ],
-        [ 'key3', 'dj', 'khaled' ],
-        [ 'key4', 'asahd', null ]
+        [ 'accessible', 'reachable', 'restricted' ],
+        [ 'service', 'assistance', 'hindrance' ],
+        [ 'productive', 'effective', 'lagging' ],
+        [ 'innovation', 'invention', null ]
       ]
+
     );
 
   });
 
   it('should not include an inner array for keys which are only present in the right object, but not the left', () => {
-    const obj1 = { key: 'anotha', key2: 'we the best', key3: 'dj', key4: 'asahd' };
-    const obj2 = { key: 'one', key2: 'music', key3: 'khaled', key5: 'leave me out' };
+    const obj1 = { accessible: 'reachable', service: 'assistance', productive: 'effective', innovation: 'invention' };
+    const obj2 = { accessible: 'restricted', service: 'hindrance', productive: 'lagging', global: 'local' };
 
     const result = leftJoin(obj1, obj2);
     expect(result).toEqual(
       [
-        [ 'key', 'anotha', 'one' ],
-        [ 'key2', 'we the best', 'music' ],
-        [ 'key3', 'dj', 'khaled' ],
-        [ 'key4', 'asahd', null ]
+        [ 'accessible', 'reachable', 'restricted' ],
+        [ 'service', 'assistance', 'hindrance' ],
+        [ 'productive', 'effective', 'lagging' ],
+        [ 'innovation', 'invention', null ]
       ]
     );
   });
